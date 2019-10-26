53e8f67a7916c23976d4f9ebb54fa85abf9d9782
realsidg <i@siddharthgarg.in>
2019-10-26 16:37:44 +0530

    Initialized Django App, Created Models

---
+++ MessManagement/Mess/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from .models import mess, student, food, order
+
+@admin.register(mess)
+class messAdmin(admin.ModelAdmin):
+    pass
+@admin.register(mess)
+class messAdmin(admin.ModelAdmin):
+    pass
+@admin.register(mess)
+class messAdmin(admin.ModelAdmin):
+    pass
+@admin.register(mess)
+class messAdmin(admin.ModelAdmin):
+    pass
+# Register your models here.
---
+++ MessManagement/Mess/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class student(models.Model):
+    Student_id = models.CharField(max_length=100, primary_key=True)
+    Name = models.CharField(max_length=100)
+    Mess_id = models.ForeignKey('mess', on_delete=models.PROTECT)
+    Password = models.CharField(max_length=100)
+    Balance = models.FloatField(max_length=100)
+
+class mess(models.Model):
+    blocks=[(chr(i),chr(i)) for i in range(65,82)]
+    Mess_id = models.CharField(max_length=50, primary_key=True)
+    Block = models.CharField(choices=blocks, max_length=1)
+    Name = models.CharField(max_length=50)
+
+class admin(models.Model):
+    Admin_id = models.CharField(max_length=20, primary_key=True)
+    Username = models.CharField(max_length=20)
+    Password = models.CharField(max_length=20)
+
+class food(models.Model):
+    Food_id= models.CharField(max_length=20, primary_key=True)
+    Name= models.CharField(max_length=20)
+    Cost= models.CharField(max_length=20)
+
+class order(models.Model):
+    Order_id= models.CharField(max_length=20, primary_key=True)
+    Food_id= models.ForeignKey("food", on_delete=models.PROTECT)
+    Time= models.DateField()
